5ee026f803b20df3f8e34d115e110d3005bda9bb
AlinaMiti <alinamitina277@gmail.com>
2023-10-12 17:47:52 +0300

    oo

--- 1-type/point/point.h
+++ 1-type/point/point.h
@@ -1,9 +1,23 @@
 bool point_in_rectangle(double x, double y) {
-    return false; // Not implementation
+    int a = 0;
+    scanf_s("%d %d", &x, &y);
+    if (x >= -5 || x <= 5 || y <= 5 || y >= -5){
+        return true;
+        a += 1;
+    }
+    else {
+        return false; // Not implementation
+    }
+    printf("%d", a);
 }
 
 bool point_in_square(double x, double y) {
-    return false; // Not implementation
+    if(((x - 6)^2 + (y - 6)^2) > 49){
+        return false; // Not implementation
+    }
+    else{
+        return true;
+    }
 }
 
 bool point_in_triangle(double x, double y) {
