5d26263ac82400f730c19c14884754d1984d6262
AlinaMiti <alinamitina277@gmail.com>
2023-10-26 17:48:40 +0300

    fg

--- 1-type/double/double.h
+++ 1-type/double/double.h
@@ -1,9 +1,21 @@
 bool compare_double(double x, double y) {
-    return true; // Not implementation
+    if(fabs(x-y)< 0,000000001){
+        return true; // Not implementation
+    }
+    else{
+        return false;
+    }
 }
 
 int get_nearest_int(double x) {
-    return 0; // Not implementation
+    if ((x - x/1) < 0.5){
+        return (x/1);
+        
+    }
+    else{
+        return (x/1 + 1);
+    }
+    
 }
 
 double get_fractional(double x) {
--- 3-loop/count_max_digit/count_max_digit.h
+++ 3-loop/count_max_digit/count_max_digit.h
@@ -1,5 +1,28 @@
 #include <math.h>
 
-char count_max_digit(long long int){
-    return 100;
+char count_max_digit(long long int num){
+    int maxi = 0;
+    int a = 0;
+    if (num < 0){
+        num = num * (-1);
+    }
+    long long int s = num;
+    long long int number = num;
+    while (number != 0){
+        int d = number%10;
+        if(d > maxi){
+            maxi = d;
+        }
+        number /= 10;
+    }
+   int k = 0;
+    while (s != 0){
+        k = s%10;
+        if( k == maxi){
+            a += 1;
+        }
+        
+        s /= 10;
+    }
+    return a;
 }
