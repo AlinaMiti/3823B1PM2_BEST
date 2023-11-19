1e40181634241e21c3873e50e4de1d86888baa39
Алина Митина <AlinaMiti>
2023-11-19 23:53:43 +0300

    ggg

--- 5-pointer/calc_size/calcsize.h
+++ 5-pointer/calc_size/calcsize.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
+
 int calcsize(void* memory){
-    return 0;
+    /*int size = 0;
+    unsigned char* arr = (unsigned char*)memory;
+    
+    while(arr[size] != 11 || arr[size+1] != 47){
+        size += 1;
+    }
+
+    return size+2;
+*/
+return 0;
 }
+
+
+
+
--- 5-pointer/scalar_product/mult.h
+++ 5-pointer/scalar_product/mult.h
@@ -1,5 +1,6 @@
 #include <math.h>
 
 int scalar_product(int* vec1, int* vec2, int size){
-    return 100;
+return 100;
+
 }
