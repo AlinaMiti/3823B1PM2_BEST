bfdba885655a4be3031667379dd15bc2f131c722
Алина Митина <AlinaMiti>
2023-12-03 22:01:09 +0300

    iii

--- 6-functions/loops/loops_finder.h
+++ 6-functions/loops/loops_finder.h
@@ -1,34 +1,30 @@
 #include <math.h>
 
 void find_loops(int* pic, int size, int* num_loop, int* start_row, int* start_col, int* finish_row, int* finish_col){
-    /*int start(int pic, int size){
+/*
     for (int i = 0; i < size; i++){
         for (int j = 0; j < size; j++){
             if (pic[j][i] == 1){
-                start_row = j;   
-                start_col = i;
-                return (start_row, start_col);
-                //continue;
-        }
-        }
+                *start_row = j;   
+                *start_col = i;   
+        }        
     }
     }
-    int loop(int pic, int size, int start_row, int start_col){
+    int petli  = 0;
     for (int f = 0; f < size-start_col; f++){
         if (pic[start_row][f] == 1 && pic[start_row-1][f]==1){
-            num_loop++;
+            petli++;
         }
     }
-    return num_loop;
-    }
-    int finisch(int pic, int start_row){
-        for(int i = 0; i < size-start_col; i++){
-            for(int t = 1; t<size-i; t++){
-                if (pic[start_row][i] == 1 && pic[start_row][i+t] == 0)
-                    return (start_row, i);
+    *num_loop = petli;
+
+        for(int i = size; i > 0; i--){
+                if (pic[start_row][i] == 0 && pic[start_row][i-1] == 1){
+
+                }
+            
             }
         }
-    }
-*/
-    return 0;
+        */
+    return;
 }
--- 6-functions/subarray/subarray_finder.h
+++ 6-functions/subarray/subarray_finder.h
@@ -1,5 +1,43 @@
 #include <math.h>
 
 int find_subarray(int* array, int array_size, int* subarray1, int subarray_size1, int* subarray2, int subarray_size2, int* subarray3, int subarray_size3){
-    return -1;
+    int index1 = -1;
+    int index2 = -1;
+    int index3 = -1;
+    for (int i = 0; i < array_size; i++){
+        if ((array[i] == subarray1[0]) && (index1 == -1)){
+            for (int j = 0; j < subarray_size1; j++){
+                if (array[i + j] != subarray1[j]){
+                    index1 = -1;
+                    break;
+                }
+                if (array[i + j] == subarray1[j]){
+                    index1 = i;
+                }         
+            }
+        }
+        if ((array[i] == subarray2[0]) && (index2 == -1)){
+            for (int j = 0; j < subarray_size2; j++){
+                if (array[i + j] != subarray2[j]){
+                    index2 = -1;
+                    break;
+                }
+                if (array[i + j] == subarray2[j]){
+                    index2 = i;
+                }         
+            }
+        }
+        if ((array[i] == subarray3[0]) && (index3 == -1)){
+            for (int j = 0; j < subarray_size3; j++){
+                if (array[i + j] != subarray3[j]){
+                    index3 = -1;
+                    break;
+                }
+                if (array[i + j] == subarray3[j]){
+                    index3 = i;
+                }         
+            }
+        }
+    }
+    return index1 + index2 + index3;
 }
