2600846dd2e7274b38b5c84277fbe65b4534cfdf
Алина Митина <AlinaMiti>
2023-12-06 23:13:30 +0300

    igg

--- 7-recursion/new_series/find_series_sum.h
+++ 7-recursion/new_series/find_series_sum.h
@@ -1,5 +1,14 @@
 #include <math.h>
 
 double find_sum_elements_series(int k){
-    return -1.0;
+    /*if (k < 0){
+        return -1;
+    }
+    if (k == 0){
+        return 0;
+    }
+    else{
+        return (double)find_sum_elements_series(k-1) + (double)(find_sum_elements_series(k - 1) + 1.0)/(double)(k);
+    }*/
+    return 1.0;
 }
