09f0a0ffd5cae6d19747c9d9b622b86caa428584
AlinaMiti <alinamitina277@gmail.com>
2023-12-16 15:32:53 +0300

    Merge branch 'main' of https://github.com/AlinaMiti/3823B1PM2_BEST

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -2,30 +2,36 @@
 
 double sum(int n){
     if (n < 1){
-        return -1;
+        return -1.0;
     }
     if (n == 1){
-        return 1;
+        return 1.0;
     }
     else{
         return 1.0/n + sum(n-1);
     }
 }
-double sum2(int n){
-    double sum = 0.0;
-    for (int i = 0; i<n; i++){
-        sum += 1.0/i;
+
+double sum2(int start, int end){
+    if (end == start){
+        return 1.0/start;
     }
-    return sum;
+    else{
+        return 1.0/end +sum2(start, end-1);
+    }
 }
 
 double sum_series(int n){
     double s = 0.0;
     if (n > 1000000){
-        double s = sum2(n);
+        int step = 1000000;
+        for (int i = 0; i < n; i += step){
+            s += sum2(i+1, i+step);
+        }
+        
     }
     else{
-         double s = sum(n);
+        s = sum(n);
     }
     return s;
 
--- 8-strings/palindrome/find_palindrom.h
+++ 8-strings/palindrome/find_palindrom.h
@@ -1,6 +1,42 @@
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
 
 int is_palindrom(char text[]){
-    return -100;
+    char str[20];
+    int j = 0;
+    for (int i = 0; i < strlen(text); i++){
+        if (text[i] != '!' && text[i] != ',' && text[i] != '\'' && text[i] != ' '){
+            str[j] = (char)tolower(text[i]);
+            j ++;
+        }
+    }
+    str[j] = '\0';
+
+
+    int len = strlen(str);
+    int a = 0;
+    if (len > 1){
+        for (int i = 0; i < len/2; i++){
+        int j = len - i - 1;
+        if (str[i] == str[j]){
+            a = 0;
+        }
+        else{
+            a = 1;
+            break;
+        }
+    }
+    }
+    else{
+        a = 0;
+    }
+
+    
+    if (a == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
